9d920eb85b2a3ed402bb50f6af91062b5cac94ee
Em-kale <emmitt.luhning@devops.ncia.nato.int>
2024-03-17 21:25:58 -0400

    added some unqiue styling for mobile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,18 @@
    import './styles.css';
     import GithubLogo from '/src/lib/icons/github-mark-white.svelte'
     import LinkedinLogo from '/src/lib/icons/linkedin-logo-svgrepo-com.svelte'
-</script>
+    import { afterUpdate} from 'svelte' 
+    
+    let titleClass="desktop-title" 
+    let subtitleClass="desktop-subtitle"  
+    afterUpdate(() => {
+        const isMobile = window.matchMedia("(max-width: 700px)")
+        if(isMobile.matches){
+             titleClass = "mobile-title";
+             subtitleClass = "mobile-subtitle";
+        }
+    })
+   </script>
 
 <svelte:head>
 	<title>Home</title>
@@ -10,14 +21,14 @@
 </svelte:head>
 
 <section >
-	<h1>Emmitt Luhning </h1>
-    <h2>Software developer, hacker, alleged android</h2>    
-    <div class="icon-container">
-    <a target="__blank" href="https://www.github.com/Em-kale"> 
-        <GithubLogo className="github-logo"/>
-    </a>
-    <a target="__blank" href="https://www.linkedin.com/in/emmittl/">
-    <LinkedinLogo className="linkedin-logo"/>
+	<h1 class={titleClass}>Emmitt Luhning </h1>
+    <h2 class={subtitleClass}>Software developer, hacker, alleged android</h2>    
+    <div class="icon-container"> 
+        <a target="__blank" href="https://www.github.com/Em-kale"> 
+            <GithubLogo className="github-logo"/>
+        </a>
+        <a target="__blank" href="https://www.linkedin.com/in/emmittl/">
+            <LinkedinLogo className="linkedin-logo"/>
     </a>
     </div>
 </section>
@@ -30,19 +41,33 @@
 		align-items: center;
 		flex: 0.6;
 	}
-  .icon-container{
-  margin: 100px 0px 0px 0px; 
-    } 
-    h2{
+    .icon-container{
+        margin: 200px 0px 0px 0px; 
+    }
+    .mobile-subtitle{
         font-size: 30px;
         font-weight: 500px;
         font-family: 'primus';
         width: 100%;
         margin: 10px 0px 0px 15px;
-        }
-	h1 {
+    }
+    .desktop-subtitle{
+        font-size: 30px;
+        font-weight: 500px;
+        font-family: 'primus';
+        width: 100%;
+        margin: 10px 0px 0px 15px;
+    }
+    .desktop-title{
         margin: 0px; 
         font-size:90px;
+        font-weight: 900px;
+        font-family: 'primus';
+        width: 100%;
+	}
+    .mobile-title{
+        margin: 0px; 
+        font-size:55px;
         font-weight: 900px;
         font-family: 'primus';
         width: 100%;
